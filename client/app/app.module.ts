7e32a70b7f548c828fee028836cc72e173723e13
Marcos Eduardo Winiarski <marcosehell@gmail.com>
2017-10-28 23:44:26 -0200

    Add
    Adicionando o component da foto.

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FotoModule } from './foto/foto.module';
 
 @NgModule({
 
   imports : [
-    BrowserModule
+    BrowserModule,
+    FotoModule
   ],
   declarations : [
     AppComponent
---
+++ client/app/foto/foto.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+
+  selector : 'foto',
+  templateUrl : './app/foto/foto.component.html'
+})
+
+export class FotoComponent {}
